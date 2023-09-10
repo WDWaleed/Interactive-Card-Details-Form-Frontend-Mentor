0cc76d1988358bd66942a28893d4a0362cd8c59e
Waleed_Ahmed <0100waleedahmed0100@gmail.com>
2023-09-10 17:05:53 +0500

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [react()],
+   base: '/Interactive_Card_Details_Form_Frontend_Mentor/'
 })
